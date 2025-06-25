6dc23488e14b68f528aa197a6b83d79917806659
sd12key <166713967+sd12key@users.noreply.github.com>
2025-06-25 20:13:23 -0230

    added Airport entity

---
+++ src/main/java/org/alvio/flightnode/rest/airport/Airport.java
@@ -0,0 +1,60 @@
+package org.alvio.flightnode.rest.airport;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.alvio.flightnode.rest.city.City;
+
+@Entity
+public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Airport name is required")
+    @Pattern(regexp = "^[a-zA-Z0-9 .'-]+$", message = "Invalid airport name")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "Airport code is required")
+    @Pattern(regexp = "^[a-zA-Z]{3}$", message = "Code must be 3 letters")
+    @Column(nullable = false, unique = true, length = 3)
+    private String code;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = (name != null) ? name.trim() : null;
+    }
+
+    public void setCode(String code) {
+        this.code = (code != null) ? code.trim().toUpperCase() : null;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+}
